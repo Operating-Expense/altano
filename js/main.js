8958b1d239c527fee12df8a98d34d75acac8c338
kitso <freshpin@gmail.com>
2018-12-13 17:31:32 +0200

    main text animation

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// function anim() {
+//     const animBlock = document.querySelector('.header__naming');
+//     setTimeout(() => {
+//         animBlock.classList.add('on-load');
+//     }, 0)
+// }
+// new anim();
+
+document.querySelector('.header__naming').classList.add('on-load');
