265345de58581d4ffb0c30a4c5115482f7146558
kitso <freshpin@gmail.com>
2018-12-14 17:14:10 +0200

    text animation, menu dropdown

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-// function anim() {
-//     const animBlock = document.querySelector('.header__naming');
-//     setTimeout(() => {
-//         animBlock.classList.add('on-load');
-//     }, 0)
-// }
-// new anim();
-
 document.querySelector('.header__naming').classList.add('on-load');
+
+
+const menuBtn = document.querySelector('.header-menu-btn');
+const menuDropdown = document.querySelector('.dropdown-menu');
+const menuDropdownClose = document.querySelector('.dropdown-menu__close');
+
+document.addEventListener('click', e => {
+    if (e.target == menuBtn) {
+        menuDropdown.classList.add('show-header-dropdown');
+    }
+    if (e.target == menuDropdownClose) {
+        menuDropdown.classList.remove('show-header-dropdown');
+    }
+})
+
+//-----slider------
+const ctrLeft = document.querySelector('head__arrow--left');
+const ctrRight = document.querySelector('head__arrow--right');
+const nextPreview = document.querySelector('head__chairimg');
+const count = document.querySelector('head__count');
+const text = document.querySelector('header__naming');
+const mainSlider = document.querySelector('head');
